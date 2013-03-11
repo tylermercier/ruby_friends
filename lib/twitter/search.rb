8b8b3c254188de14d8ed3132bf933c9937161e70
Tyler Mercier <tylermercier@gmail.com>
2013-03-10 20:23:42 -0600

    cleanup twitter code

--- lib/twitter/search.rb
+++ lib/twitter/search.rb
@@ -2,9 +2,8 @@ require "#{File.dirname(__FILE__)}/twitter_user"
 
 class Search
   def initialize(twitter_response)
-    @tweets = []
-    twitter_response[:statuses].each do |tweet|
-      @tweets << TwitterUser.parse(tweet)
+    @tweets = twitter_response[:statuses].map do |tweet|
+      TwitterUser.parse(tweet)
     end
   end
 
