34561e45a547c3d360d08ea6d8d13c8cc9128bf0
Tyler Mercier <tylermercier@gmail.com>
2013-03-10 20:17:04 -0600

    cleanup api code

--- app/routes/api_routes.rb
+++ app/routes/api_routes.rb
@@ -1,30 +1,35 @@
 class MyApp < Sinatra::Base
-  get "/api/search" do
+  post "/api/twitter_auth" do
+    save_user_session(params[:authString])
+    redirect "/api/feed"
+  end
+
+  post "/api/feed" do
+    save_user_session(params[:authString])
     client = Authorization.twitter_client(@current_user)
-    result = client.search params[:q]
-    content_type :json
-    { search_term: params[:q], statuses: Search.new(result)}.to_json
+    json_response Following.new(client.home_timeline)
   end
 
   get "/api/feed" do
     client = Authorization.twitter_client(@current_user)
-    following = Following.new(client.home_timeline)
-    content_type :json
-    following.to_json
+    json_response Following.new(client.home_timeline)
   end
 
-  post "/api/feed" do
-    user = User.parse_from_auth_string(params[:authString])
-    session[:user] = user
-    client = Authorization.twitter_client(user)
-    following = Following.new(client.home_timeline)
-    content_type :json
-    following.to_json
+  get "/api/search" do
+    client = Authorization.twitter_client(@current_user)
+    search_term = params[:q] || 'moodyfriends'
+    result = client.search(search_term)
+    json_response search_term: search_term, statuses: Search.new(result)
   end
 
-  post "/api/twitter_auth" do
-    user = User.parse_from_auth_string(params[:authString])
+  def save_user_session(auth_string)
+    user = User.parse_from_auth_string(auth_string)
     session[:user] = user
-    redirect "/api/feed"
+    @current_user = user
+  end
+
+  def json_response(object)
+    content_type :json
+    object.to_json
   end
 end
