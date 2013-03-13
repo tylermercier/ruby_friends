b1c51e83f5d3339d04ccd41b80895833c89bc895
Tyler Mercier <tylermercier@gmail.com>
2013-03-12 19:53:09 -0600

    add production config

--- app.rb
+++ app.rb
@@ -38,6 +38,19 @@ class MyApp < Sinatra::Base
     ActiveRecord::Base.establish_connection(RACK_ENV)
   end
 
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'])
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :port     => db.port,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
+
   Dir["./app/helpers/*.rb"].each { |file| require file }
   Dir["./app/models/*.rb"].each { |file| require file }
   Dir["./app/routes/*.rb"].each { |file| require file }
