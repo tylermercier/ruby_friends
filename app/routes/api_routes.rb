f1012859635e39bd3d49b2ae1a3272438651d846
Tyler Mercier <tylermercier@gmail.com>
2013-03-16 15:59:02 -0600

    add sentiment to search results

--- app/routes/api_routes.rb
+++ app/routes/api_routes.rb
@@ -7,19 +7,21 @@ class MyApp < Sinatra::Base
   post "/api/feed" do
     save_user_session(params)
     client = Authorization.twitter_client(@current_user)
-    json_response Following.new(client.home_timeline(count: 200))
+    result = client.home_timeline(count: 200)
+    json_response Following.new(result)
   end
 
   get "/api/feed" do
     client = Authorization.twitter_client(@current_user)
-    json_response Following.new(client.home_timeline(count: 200))
+    result = client.home_timeline(count: 200)
+    json_response Following.new(result)
   end
 
   get "/api/search" do
     client = Authorization.twitter_client(@current_user)
     search_term = params[:q] || 'moodyfriends'
     result = client.search(search_term, count: 200)
-    json_response search_term: search_term, statuses: Search.new(result)
+    json_response Search.new(result, search_term)
   end
 
   def save_user_session(params)
--- lib/twitter/search.rb
+++ lib/twitter/search.rb
@@ -1,13 +1,22 @@
 require "#{File.dirname(__FILE__)}/twitter_user"
 
 class Search
-  def initialize(twitter_response)
+  def initialize(twitter_response, search_term)
+    @search_term = search_term
+    @sentiment = 0
     @tweets = twitter_response[:statuses].map do |tweet|
-      TwitterUser.parse(tweet)
+      tweet = TwitterUser.parse(tweet)
+      @sentiment += tweet[:sentiment]
+      tweet
     end
+    @sentiment = @sentiment / @tweets.length
   end
 
   def to_json
-    @tweets.to_json
+    {
+      search_term: @search_term,
+      sentiment: @sentiment,
+      statuses: @tweets
+    }.to_json
   end
 end
